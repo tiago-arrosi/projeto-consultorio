12666850c465d826cac013721cbfc47407b6cf56
Tiago Arrosi <tiago.arrosi@totvs.com.br>
2021-04-27 15:55:37 -0300

    criando DTO para trafegar dados entre as camadas

--- src/main/java/br/com/projeto/consultorio/controller/EntradasController.java
+++ src/main/java/br/com/projeto/consultorio/controller/EntradasController.java
@@ -3,25 +3,23 @@ package br.com.projeto.consultorio.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import br.com.projeto.consultorio.orm.Receita;
+import br.com.projeto.consultorio.controller.dto.ReceitaDto;
 
-@Controller
+@RestController
 public class EntradasController {
 
 	@RequestMapping("/receitas")
-	@ResponseBody
-	public List<Receita> lista() {
-		Receita receita = new Receita();
+	public List<ReceitaDto> lista() {
+		ReceitaDto receita = new ReceitaDto();
 		receita.setId(1);
 		receita.setDescricao("Consulta Particular");
 		
-		Receita receita2 = new Receita();
+		ReceitaDto receita2 = new ReceitaDto();
 		receita2.setId(2);
-		receita2.setDescricao("Consulta Plano Unimed");
+		receita2.setDescricao("Consulta Plano Empresarial Unimed");
 		
 		return Arrays.asList(receita, receita2);
 	}
